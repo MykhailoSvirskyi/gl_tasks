344737cb88aa08a52aba8e3c3109ecef9d403e96
MykhailoSvirskyi <90723683+MykhailoSvirskyi@users.noreply.github.com>
2022-12-30 14:34:24 +0200

    Update task1.c

--- PURE1/task1.c
+++ PURE1/task1.c
@@ -2,8 +2,6 @@
 
 void main()
 {
-
     printf("hello world!\nGroup number 1\nMykhailo Svirskyi\nmihail.svirskiy@gmail.com\nhttps://github.com/MykhailoSvirskyi/gl_tasks.git\nhttps://drive.google.com/drive/folders/1or1Z_sdEfNxB-auJuTuOm-qgTA_CXDWq?usp=share_link");
-
     return 0;
 }
